85ae050d57b9ea903169f31b5dd5b9aeeed7ed61
Amir <aamirali078@.com>
2023-04-01 22:19:10 +0500

    Stirng

--- String/StringWorld.java
+++ String/StringWorld.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
 import java.util.Scanner;
-public class StringWolrd{
+class StringWolrd{
 	String s;
 	String s2;
 	public void reverseText(String s) {
@@ -26,63 +25,79 @@ public class StringWolrd{
 		}
 	}
 	public void checkAnagram(String s, String s2) {
-		int n1=s.length();
-		int n2=s2.length();
-		s=s.toLowerCase();
-		s2=s2.toLowerCase();
-		if(n1==n2) {
-			char[] text1=s.toCharArray();
-			char[] text2=s2.toCharArray();
-			Arrays.sort(text1);
-			Arrays.sort(text2);
-		if(Arrays.equals(text1, text2)){
-			System.out.println(s+" And "+s2+" is Anagram");
-		}
-		else {
-			System.out.println(s+" And "+s2+" is not Anagram");
-		}
-		}
-		else {
-			System.out.println(s+" And "+s2+" is not Anagram");
-		}
-	}
-	public void diagnoss(String s) {
+		this.s=s;
+		this.s2=s2;
+		if(this.s.length()==this.s2.length()) {
+       char a1[] = s.toCharArray();
+       char a2[] = s2.toCharArray();
+		for(int i = 0 ; i < s.length();i++)
+ {
+  for(int j = i+1 ; j< s.length();j++)
+ {
+      if(a1[i]>a1[j])
+  {
+ char temp = a1[i];
+ a1[i] = a1[j];
+ a1[j] = temp;
+            }			
+		}}
+for(int i = 0 ; i < s2.length();i++)
+    {
+     for(int j = i+1; j< s2.length();j++)
+  {
+    if(a2[i] >a2[j])
+     {
+    char temp1 = a2[i];
+    a2[i] = a2[j];
+    a2[j] = temp1;
+	}}}
+String world1 = new String(s.toCharArray());
+ String world2 = new String(s2.toCharArray());
+    if(world1.equals(world2)){
+    System.out.println(s+" And "+s2+" are Anagram");
+   }
+else{
+  System.out.println(s+" And "+s2+" are Not Anagram");
+  }
+	}}
+	
+ public void diagnoss(String s) {
 		int size=s.length();
-		int vowels=0;
 		int consonant=0;
 		int space=0;
 		s=s.toLowerCase();
-		for(int i=0; i<s.length(); i++) {
-			char ch=s.charAt(i);
-			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
-				vowels++;
-			}
-			else if(ch>='a' && ch<='z') {
-				consonant++;
-			}
-			else if(ch==' ') {
-				space++;
-			}
-		}
-		System.out.println("No of Vowels: "+vowels);
-		System.out.println("No of Consonants: "+consonant);
-		System.out.println("No spaces in text: "+space);
-		System.out.println("size of the String is: "+size);
-		
-	}
-	public void frequencyOfLetters(String s,char ch) {
-		int frequency=0;
-		for(int i=0; i<s.length(); i++) {
-			if(ch==s.charAt(i)) {
-				frequency++;
-			}
-		}
-		System.out.println("Frequency of "+ch+" = "+frequency);
-	}
-	  
+         String vowels = "aeiouAEIOU";
+         String uniqueVowels = "";
+          int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (vowels.indexOf(c) != -1 && uniqueVowels.indexOf(c) == -1) {
+            uniqueVowels += c;
+            count++;
+        }
+    }
+         System.out.println(count);
+}
+
+	public void frequencyOfLetters(String s) {
+		int[] freq = new int[26];
+        s = s.toLowerCase();
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (c >= 'a' && c <= 'z') {
+			freq[c]++;
+           // freq[c - 'a']++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            System.out.printf("%c: %d%n", 'a' + i, freq[i]);
+        }
+    }
+}
+	 
 	public static void main(String[] args) {
 		String s;
-		char ch;
 		String s2;
 		int choice;
 		System.out.println("Select the Operation you wanted to Perform: ");
@@ -119,9 +134,7 @@ public class StringWolrd{
 		{
 			System.out.print("enter the text: ");
 			s=sc.nextLine();
-			System.out.print("enter the character you wants to know the frequency: ");
-			ch=sc.nextLine().charAt(0);
-			r.frequencyOfLetters(s, ch);
+			r.frequencyOfLetters(s);
 		}
 		else {
 			System.out.println("Invalid Operation or text");
