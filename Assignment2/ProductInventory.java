d382476d97b42003bfc99b75b15ea49e4380b2b8
Amir Ali <123484766+AmirAli078@users.noreply.github.com>
2023-06-07 20:43:03 +0500

    Update ProductInventory.java

--- Assignment2/ProductInventory.java
+++ Assignment2/ProductInventory.java
@@ -38,39 +38,4 @@ public class ProductInventory {
 	   }
    }
    }
-   public static void main(String args[]) {
-	   ProductInventory inventary=new ProductInventory();
-	   Product product6=new Product("Ecofriendly","organic",25.5,"clothing",true);
-	   Product product1 = new Product("Eco T-Shirt", "OrganicWear", 25.99, "Clothing", true);
-       Product product2 = new Product("Eco Jeans", "Levi's", 89.99, "Clothing", true);
-       Product product3 = new Product("Shampoo", "Pantene", 8.99, "Skincare", false);
-       Product product4 = new Product("Moisturizer", "Olay", 12.49, "Skincare", false);
-       Product product5 = new Product("All-Purpose Cleaner", "Clorox", 4.99, "Cleaning", true);
-	   String[] targetConcer= {"none"};
-       Product product7=new SkincareProduct("Moisturize", "Olay",12.49,"Skincare No	Skin Type: Sensitive", false, "none",targetConcer, false);
-       Product product8=new CleaningProduct("Laundry Detergent", "Tide", 12.5, "Cleaning", true,"Clothing", "Fresh", false);
-       Product product9=new ElectronicProduct("Smart Thermostat","Nest",249.99,"Electronic",true," Energy Efficient: Yes, Smart", true, true);
-       inventary.addProduc(product1);
-       inventary.addProduc(product1);
-       product1.print_Info();
-       inventary.addProduc(product2);
-       product2.print_Info();
-       inventary.addProduc(product3);
-       product3.print_Info();
-       inventary.addProduc(product4);
-       product4.print_Info();
-       inventary.addProduc(product5);
-       product5.print_Info();
-       inventary.addProduc(product6);
-       product6.print_Info();
-	    inventary.addProduc(product7);
-       product6.print_Info();
-       inventary.addProduc(product8);
-       product8.print_Info();
-       inventary.addProduc(product9);
-       product9.print_Info();
-	  inventary.searchProduct("Shampoo");
-      inventary.removeProduct(product1);
-	  
-   }
 }
